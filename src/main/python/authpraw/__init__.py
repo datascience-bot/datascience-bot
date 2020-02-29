4c40ccbd3b0edd1af553f0d598dfdd0242f17ed0
vogt4nick <vogt4nick@gmail.com>
2020-02-29 07:50:37 -0500

    actually omit extra users from authpraw

--- src/main/python/authpraw/__init__.py
+++ src/main/python/authpraw/__init__.py
@@ -15,21 +15,3 @@ def get_datascience_bot():
     )
 
 
-def get_substantial_strain6():
-    return praw.Reddit(
-        username=os.getenv("SUBSTANTIALSTRAIN6_USERNAME"),
-        password=os.getenv("SUBSTANTIALSTRAIN6_PASSWORD"),
-        client_id=os.getenv("SUBSTANTIALSTRAIN6_CLIENT_ID"),
-        client_secret=os.getenv("SUBSTANTIALSTRAIN6_CLIENT_SECRET"),
-        user_agent="u/SubstantialStrain6",
-    )
-
-
-def get_b3405920():
-    return praw.Reddit(
-        username=os.getenv("B3405920_USERNAME"),
-        password=os.getenv("B3405920_PASSWORD"),
-        client_id=os.getenv("B3405920_CLIENT_ID"),
-        client_secret=os.getenv("B3405920_CLIENT_SECRET"),
-        user_agent="u/b3405920",
-    )
