52f814e089b4f2e700abff4a8f61400e4f70d5fe
vogt4nick <vogt4nick@gmail.com>
2020-03-22 08:34:40 +0800

    upgrade authpraw tests

---
+++ libs/authpraw/tests/test_authpraw.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""Test authpraw.py module
+"""
+import unittest
+
+from authpraw import get_datascience_bot
+
+
+class TestGetUser(unittest.TestCase):
+    """Unit test expected behavior to get user profiles
+    """
+
+    def test_get_datascience_bot(self):
+        # redditor = get_datascience_bot()
+        # self.assertEqual(redditor.user.me(), "datascience-bot")
+
+        import os
+
+        self.assertEqual(os.getenv("DATASCIENCE_BOT_USERNAME"), "datascience-bot")
+
+
+if __name__ == "__main__":
+    unittest.main()
