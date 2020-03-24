012925adb0741657afaeffa48b84ddafac6e7a83
vogt4nick <vogt4nick@gmail.com>
2020-03-25 07:16:26 +0800

    init monitor lib

---
+++ libs/shared/monitor/monitor/__init__.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""Monitor activity on a subreddit
+"""
+from abc import ABC, abstractmethod
+import os
+from typing import Generator
+
+import praw
+
+
+class AbstractMonitor(ABC):
+    """Abstract class to derive content-specific monitors
+    """
+
+    def __init__(self, reddit: praw.models.reddit):
+        subreddit_name = os.getenv("SUBREDDIT_NAME")
+        # TODO: error if SUBREDDIT_NAME is not defined
+        self.subreddit = reddit.subreddit(subreddit_name)
+
+    @abstractmethod
+    def stream(self) -> Generator[praw.models.Submission, None, None]:
+        """Stream content-specific activities
+        """
+        pass
+
+
+class CommentMonitor(AbstractMonitor):
+    """Monitor comments on a subreddit
+    """
+
+    def stream(self) -> Generator[praw.models.Submission, None, None]:
+        yield from self.subreddit.stream.comments()
+
+
+class SubmissionMonitor(AbstractMonitor):
+    """Monitor submissions on a subreddit
+    """
+
+    def stream(self) -> Generator[praw.models.Submission, None, None]:
+        yield from self.subreddit.stream.submissions()
---
+++ libs/shared/monitor/tests/test_monitor.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""Test expected behavior of monitors
+"""
+import abc
+from typing import Type
+import unittest
+
+import praw
+
+from authpraw import get_datascience_bot
+from monitor import CommentMonitor, SubmissionMonitor
+
+
+class BaseTestCase(abc.ABC):
+    # TODO: How to type hint monitor_constructor?
+    # This isn't quite right: Union[Type[CommentMonitor], Type[SubmissionMonitor]]
+    @abc.abstractmethod
+    def setUp(self, monitor_constructor):
+        self.monitor = monitor_constructor(get_datascience_bot())
+
+    def tearDown(self):
+        del self.monitor
+
+    @abc.abstractmethod
+    def test_stream(self, assert_type: Type):
+        for i, obj in enumerate(self.monitor.stream()):
+            break
+        self.assertIsInstance(obj, assert_type)
+
+
+class TestCommentMonitor(BaseTestCase, unittest.TestCase):
+    def setUp(self):
+        super().setUp(CommentMonitor)
+
+    def test_stream(self):
+        super().test_stream(assert_type=praw.models.Comment)
+
+
+class TestSubmissionMonitor(BaseTestCase, unittest.TestCase):
+    def setUp(self):
+        super().setUp(SubmissionMonitor)
+
+    def test_stream(self):
+        super().test_stream(assert_type=praw.models.Submission)
+
+
+if __name__ == "__main__":
+    unittest.main()
