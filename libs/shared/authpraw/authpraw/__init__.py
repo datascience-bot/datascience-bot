463bc403e975fab30df5aa85ffc32852baf868ad
vogt4nick <vogt4nick@gmail.com>
2020-03-25 02:25:59 +0800

    add SubstantialStrain6 and b3405920 users to authpraw

--- libs/shared/authpraw/authpraw/__init__.py
+++ libs/shared/authpraw/authpraw/__init__.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
-"""Authenticate u/datascience-bot with praw
+"""Authenticate praw sessions for
+- u/datascience-bot
+- u/SubstantialStrain6
+- u/b3405920
 """
 import os
 import praw
@@ -12,4 +15,24 @@ def get_datascience_bot():
         client_id=os.getenv("DATASCIENCE_BOT_CLIENT_ID"),
         client_secret=os.getenv("DATASCIENCE_BOT_CLIENT_SECRET"),
         user_agent="u/datascience-bot",
+    )
+
+
+def get_SubstantialStrain6():
+    return praw.Reddit(
+        username=os.getenv("SUBSTANTIALSTRAIN6_USERNAME"),
+        password=os.getenv("SUBSTANTIALSTRAIN6_PASSWORD"),
+        client_id=os.getenv("SUBSTANTIALSTRAIN6_CLIENT_ID"),
+        client_secret=os.getenv("SUBSTANTIALSTRAIN6_CLIENT_SECRET"),
+        user_agent="u/SubstantialStrain6",
+    )
+
+
+def get_b3405920():
+    return praw.Reddit(
+        username=os.getenv("B3405920_USERNAME"),
+        password=os.getenv("B3405920_PASSWORD"),
+        client_id=os.getenv("B3405920_CLIENT_ID"),
+        client_secret=os.getenv("B3405920_CLIENT_SECRET"),
+        user_agent="u/b3405920",
     )
--- libs/shared/authpraw/tests/test_authpraw.py
+++ libs/shared/authpraw/tests/test_authpraw.py
@@ -3,7 +3,7 @@
 """
 import unittest
 
-from authpraw import get_datascience_bot
+from authpraw import get_datascience_bot, get_SubstantialStrain6, get_b3405920
 
 
 class TestGetUser(unittest.TestCase):
@@ -13,6 +13,14 @@ class TestGetUser(unittest.TestCase):
     def get_datascience_bot_test(self):
         redditor = get_datascience_bot()
         self.assertEqual(redditor.user.me(), "datascience-bot")
+
+    def get_SubstantialStrain6_test(self):
+        redditor = get_SubstantialStrain6()
+        self.assertEqual(redditor.user.me(), "SubstantialStrain6")
+
+    def get_b3405920_test(self):
+        redditor = get_b3405920()
+        self.assertEqual(redditor.user.me(), "b3405920")
 
 
 if __name__ == "__main__":
