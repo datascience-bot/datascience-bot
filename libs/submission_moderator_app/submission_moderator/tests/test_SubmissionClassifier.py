774f2c02535311a8d5a1f8d5d674fa5bd0837996
vogt4nick <vogt4nick@gmail.com>
2020-03-30 00:28:20 +0800

    shorten names in test_SubmissionClassifier.py

--- libs/submission_moderator_app/submission_moderator/tests/test_SubmissionClassifier.py
+++ libs/submission_moderator_app/submission_moderator/tests/test_SubmissionClassifier.py
@@ -13,41 +13,41 @@ class SubmissionClassifierTest(BaseTestCase):
 
     def setUp(self):
         super().setUp()
-        self.submission_classifier = SubmissionClassifier()
+        self.classifier = SubmissionClassifier()
 
     def tearDown(self):
         super().setUp()
-        del self.submission_classifier
+        del self.classifier
 
     def test_classify_blog(self):
         """Unit test expected behavior of SubmissionClassifier.classify
         """
         self.submission.url = "https://medium.com/asdfasdf"
-        self.submission_classifier.classify(self.submission)
+        self.classifier.classify(self.submission)
 
-        assert self.submission_classifier.is_porn is False
-        assert self.submission_classifier.is_video is False
-        assert self.submission_classifier.is_blog is True
+        assert self.classifier.is_porn is False
+        assert self.classifier.is_video is False
+        assert self.classifier.is_blog is True
 
     def test_classify_porn(self):
         """Unit test expected behavior of SubmissionClassifier.classify
         """
         self.submission.url = "https://pornhub.com/asdf"
-        self.submission_classifier.classify(self.submission)
+        self.classifier.classify(self.submission)
 
-        assert self.submission_classifier.is_porn is True
-        assert self.submission_classifier.is_video is False
-        assert self.submission_classifier.is_blog is False
+        assert self.classifier.is_porn is True
+        assert self.classifier.is_video is False
+        assert self.classifier.is_blog is False
 
     def test_classify_video(self):
         """Unit test expected behavior of SubmissionClassifier.classify
         """
         self.submission.url = "https://www.youtube.com/watch?v=-qstIEHDVRg"
-        self.submission_classifier.classify(self.submission)
+        self.classifier.classify(self.submission)
 
-        assert self.submission_classifier.is_porn is False
-        assert self.submission_classifier.is_video is True
-        assert self.submission_classifier.is_blog is False
+        assert self.classifier.is_porn is False
+        assert self.classifier.is_video is True
+        assert self.classifier.is_blog is False
 
 
 if __name__ == "__main__":
