a69e5b89c6f6ee8dcd4adf8816765c4c4238987b
vogt4nick <vogt4nick@gmail.com>
2020-03-30 01:52:33 +0800

    init travis-container-script

---
+++ travis-container-script.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+export BAZEL_FLAGS="--discard_analysis_cache --nokeep_state_after_build --notrack_incremental_state"
+export OUTPUT_DIR=_output
+
+mkdir -p $OUTPUT_DIR
+
+bazel build //... ${BAZEL_FLAGS}
+bazel test //... ${BAZEL_FLAGS}
+
+# make binaries available to volume mounted to docker container
+cp -f bazel-bin/apps/submission_moderator_app/bin.zip ${OUTPUT_DIR}/bin.zip
