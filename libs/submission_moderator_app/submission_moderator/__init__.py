64188256996eb94cb7407d5d5b3c2a045628dd42
Nick Vogt <31822672+vogt4nick@users.noreply.github.com>
2020-03-30 21:30:12 -0400

    split submission_classifier from submission_moderator (#12)
    * migrate submission_classifier to its own project
    
    * remove flask, packaging, waitress dependencies
    
    * init submission_moderator_app package group
    
    * minor docstring fixes

--- libs/submission_moderator_app/submission_moderator/__init__.py
+++ libs/submission_moderator_app/submission_moderator/__init__.py
@@ -1,16 +1,19 @@
 # -*- coding: utf-8 -*-
-"""Submission Moderator for r/datascience
+"""Submission Moderator
 
-Identify submissions from under-qualified users and links to spam.
+Moderate submissions from new redditors and links to spam.
 
 Usage:
-    >>> import authpraw
+    >>> import libs.shared.authpraw
+    >>> from libs.submission_moderator_app.submission_moderator import SubmissionModerator
     >>> reddit = authpraw.get_datascience_bot()
-    >>> sm = SubmissionModerator(reddit)
+    >>> moderator = SubmissionModerator(reddit)
     >>> submission = reddit.submission("euot0h")
-    >>> sm.moderate(submission)
+    >>> moderator.moderate(submission)
 """
 import praw
+
+from libs.submission_moderator_app.submission_classifier import SubmissionClassifier
 
 
 class SubmissionModerator:
@@ -70,57 +73,3 @@ class SubmissionModerator:
             return None
 
         self.submission.mod.approve()
-
-
-class SubmissionClassifier:
-    """Classify a submission as one or more of the following:
-
-        1. porn,
-        2. a banned video hosting site, or
-        3. a banned blog aggregator
-    """
-
-    VIDEO_DOMAINS = ("youtube.com", "youtu.be", "vid.me")
-    BLOG_DOMAINS = ("towardsdatascience.com", "medium.com")
-    # TODO: What about valid blogs hosted on medium? e.g. medium.com/netflix-techblog
-    PORN_DOMAINS = (
-        "porn.com",
-        "pornhub.com",
-        "porntube.com",
-        "redtube.com",
-        "socialmunch.com",
-        "spankwire.com",
-        "xhamster.com",
-        "xvideos.com",
-        "youjizz.com",
-        "youporn.com",
-        "extremetube.com",
-        "hardsextube.com",
-    )
-    min_karma = 50
-
-    def __init__(self):
-        self.is_blog: bool = False
-        self.is_porn: bool = False
-        self.is_troll: bool = False
-        self.is_video: bool = False
-
-    def classify(self, submission: praw.models.Submission) -> None:
-        """Classify a submission as one or more labels
-
-        Args:
-            submission (praw.models.Submission): Submission to classify
-        """
-        self.__init__()
-        self.submission = submission
-
-        # for more readable list comprehensions
-        author = self.submission.author
-        domain = self.submission.domain
-
-        self.is_troll = author.comment_karma + author.link_karma < self.min_karma
-        self.is_blog = any(blog_domain == domain for blog_domain in self.BLOG_DOMAINS)
-        self.is_porn = any(porn_domain == domain for porn_domain in self.PORN_DOMAINS)
-        self.is_video = any(
-            video_domain == domain for video_domain in self.VIDEO_DOMAINS
-        )
