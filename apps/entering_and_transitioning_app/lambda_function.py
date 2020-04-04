52584939fff0cd7c668c06367f93ff678ed839c1
vogt4nick <vogt4nick@gmail.com>
2020-04-05 07:35:37 +0800

    add params to lambda_handler

--- apps/entering_and_transitioning_app/lambda_function.py
+++ apps/entering_and_transitioning_app/lambda_function.py
@@ -1,8 +1,16 @@
 # -*- coding: utf-8 -*-
 """Entrypoint for AWS Lambda function
 """
+from datetime import datetime
 from apps.entering_and_transitioning_app import main
 
 
 def lambda_handler(event, context):
-    main()
+    try:
+        time_string = event["body"]["time"]
+        validate = event["body"]["validate"]
+    except KeyError:
+        main()
+    else:
+        time = datetime.strptime(time_string, "%Y-%m-%d")
+        main(time=time, validate=validate)
