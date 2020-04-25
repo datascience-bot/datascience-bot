09c8ad8d4e08b8a4261b6a7be88d51cdf12fe8f6
vogt4nick <vogt4nick@gmail.com>
2020-04-26 02:52:38 +0800

    remove bazel flags from bazel build command

--- get-build-artifacts.sh
+++ get-build-artifacts.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # make artifacts available to volume mounted to docker container
 export OUTPUT_DIR=_output
-bazel build //... ${BAZEL_FLAGS}
+bazel build //...
 cp -f -a bazel-bin/apps ${OUTPUT_DIR}
