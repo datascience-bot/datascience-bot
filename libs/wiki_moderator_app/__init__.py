72d53a2441bca83c11f61303a7bdba112671471f
vogt4nick <vogt4nick@gmail.com>
2020-04-26 05:03:45 +0800

    pass subreddit_name to wiki_moderator_app binary

--- libs/wiki_moderator_app/__init__.py
+++ libs/wiki_moderator_app/__init__.py
@@ -1,17 +1,17 @@
+import logging
 import os
 import pathlib
 
 from libs.shared.authpraw import get_datascience_bot
 from libs.shared.modwiki import get_local_wiki, update_wiki
 
-
-SUBREDDIT_NAME = os.getenv("SUBREDDIT_NAME")
 WIKI_DIR = pathlib.Path("libs/wiki_moderator_app/wiki")
+logger = logging.getLogger(__name__)
 
 
-def main():
+def main(subreddit_name: str):
     bobby = get_datascience_bot()
-    remote_wiki = bobby.subreddit(SUBREDDIT_NAME).wiki
+    remote_wiki = bobby.subreddit(subreddit_name).wiki
     local_wiki = get_local_wiki(WIKI_DIR)
 
     update_wiki(remote_wiki, local_wiki)
