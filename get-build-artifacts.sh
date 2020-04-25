7240942608d6dea100340a85ed4e4ce38089ebb6
vogt4nick <vogt4nick@gmail.com>
2020-04-26 01:37:34 +0800

    order build artifacts alphabetically

--- get-build-artifacts.sh
+++ get-build-artifacts.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 # export BAZEL_FLAGS="--discard_analysis_cache --nokeep_state_after_build --notrack_incremental_state"
 export OUTPUT_DIR=_output
-export SUBMISSION_MODERATOR_APP_DIR=${OUTPUT_DIR}/submission_moderator_app
 export ENTERING_AND_TRANSITIONING_APP_DIR=${OUTPUT_DIR}/entering_and_transitioning_app
+export SUBMISSION_MODERATOR_APP_DIR=${OUTPUT_DIR}/submission_moderator_app
 export WIKI_MODERATOR_APP_DIR=${OUTPUT_DIR}/wiki_moderator_app
 
 # mkdir -p $OUTPUT_DIR
-mkdir -p $SUBMISSION_MODERATOR_APP_DIR
 mkdir -p $ENTERING_AND_TRANSITIONING_APP_DIR
+mkdir -p $SUBMISSION_MODERATOR_APP_DIR
 mkdir -p $WIKI_MODERATOR_APP_DIR
 
 bazel build //... ${BAZEL_FLAGS}
 
 # make artifacts available to volume mounted to docker container
-cp -f -r bazel-bin/apps/submission_moderator_app/lambda_pkg.zip ${SUBMISSION_MODERATOR_APP_DIR}/lambda_pkg.zip
 cp -f -r bazel-bin/apps/entering_and_transitioning_app/lambda_pkg.zip ${ENTERING_AND_TRANSITIONING_APP_DIR}/lambda_pkg.zip
+cp -f -r bazel-bin/apps/submission_moderator_app/lambda_pkg.zip ${SUBMISSION_MODERATOR_APP_DIR}/lambda_pkg.zip
 cp -f -r bazel-bin/apps/wiki_moderator_app/bin ${WIKI_MODERATOR_APP_DIR}/bin
