81ce7df8032c24713757ae0deb4d844a61335234
vogt4nick <vogt4nick@gmail.com>
2020-04-26 18:29:27 +0800

    allow lambda function params

--- apps/entering_and_transitioning_app/lambda_function.py
+++ apps/entering_and_transitioning_app/lambda_function.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 """Entrypoint for AWS Lambda function
 """
+from datetime import datetime
 import os
 
 from libs.entering_and_transitioning_app import main
@@ -11,4 +12,20 @@ SUBREDDIT_NAME: str = os.getenv("SUBREDDIT_NAME")
 
 
 def lambda_handler(event, context):
-    main(reddit=get_datascience_bot(), subreddit_name=SUBREDDIT_NAME)
+    # TODO handle what happens if there's not an existing thread
+    try:
+        time_string = event["body"]["time"]
+        validate = event["body"]["validate"]
+    except KeyError:
+        main(reddit=get_datascience_bot(), subreddit_name=SUBREDDIT_NAME)
+    else:
+        time = datetime.strptime(time_string, "%Y-%m-%d")
+        main(
+            reddit=get_datascience_bot(),
+            subreddit_name=SUBREDDIT_NAME,
+            time=time,
+            validate=validate,
+        )
+
+    return "200 OK"
+
