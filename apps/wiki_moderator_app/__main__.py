c2dda58b0185e0dc5e7e2c8cf8b3f311b3f4f6b1
vogt4nick <vogt4nick@gmail.com>
2020-04-26 08:24:00 +0800

    consolidate logic to __main__.py

--- apps/wiki_moderator_app/__main__.py
+++ apps/wiki_moderator_app/__main__.py
@@ -2,12 +2,20 @@
 """wiki_moderator_app binary
 """
 import argparse
+import logging
 
-from apps.wiki_moderator_app import main
+from libs.wiki_moderator_app import main
+
+
+logging.basicConfig(
+    format=("%(asctime)s.%(msecs)03d UTC | %(levelname)-8s | %(message)s"),
+    level=logging.INFO,
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+logger = logging.getLogger(__name__)
 
 
 parser = argparse.ArgumentParser(description="Update the wiki on a subreddit")
-
 parser.add_argument(
     "subreddit_name",
     metavar="subreddit_name",
